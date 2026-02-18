b769288227f44dacce0d4f7aeca297d597813ac6
Vivek Gautam <vivekbgautam@gmail.com>
2026-02-19 04:42:00 +0530

    Add files via upload

---
+++ Recursion_programming_Practice-Code/program699.cpp
@@ -0,0 +1,26 @@
+// Input  : 5
+// Output : 1   2   3   4   5
+// final
+
+#include<iostream>
+using namespace std;
+
+void Display(int iNo)
+{
+    if(iNo >=1)
+    {
+        cout<<iNo<<"\t";
+        Display(iNo - 1 );
+        cout<<iNo<<"\t";
+    }   
+}
+int main()
+{ int iValue = 0;
+
+    cout<<"Enter the number :\n";
+    cin>>iValue;
+
+    Display(iValue);
+    cout<<"\n";
+    return 0;
+}
---
+++ Recursion_programming_Practice-Code/program700.cpp
@@ -0,0 +1,30 @@
+// Input  : 7891
+// Output : $
+// final
+
+#include<iostream>
+using namespace std;
+
+int CountDigits(int iNo)
+{
+    int iCount = 0;
+    while(iNo != 0)
+    {
+        iCount++;
+        iNo = iNo / 10;
+    }   
+
+    return iCount;
+}
+int main()
+{ 
+    int iValue = 0;
+    int iRet = 0;
+
+    cout<<"Enter the number :\n";
+    cin>>iValue;
+
+    iRet = CountDigits(iValue);
+    cout<<"Number of digits are :"<<iRet<<"\n";
+    return 0;
+}
