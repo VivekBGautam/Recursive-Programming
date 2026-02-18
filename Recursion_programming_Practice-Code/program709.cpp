227ce6396bd8f649bccaeeb889e0c6e1871a3740
Vivek Gautam <vivekbgautam@gmail.com>
2026-02-19 04:43:13 +0530

    Add files via upload

---
+++ Recursion_programming_Practice-Code/program709.cpp
@@ -0,0 +1,30 @@
+// Input  : 7891
+// Output : $
+
+#include<iostream>
+using namespace std;
+
+int SumDigits(int iNo)
+{
+    static int iSum = 0;
+
+    if(iNo != 0)
+    {
+        iSum = iSum + (iNo % 10);  // without iDigits
+        SumDigits(iNo / 10);
+    }   
+
+    return iSum;
+}
+int main()
+{ 
+    int iValue = 0;
+    int iRet = 0;
+
+    cout<<"Enter the number :\n";
+    cin>>iValue;
+
+    iRet = SumDigits(iValue);
+    cout<<"Sum of digits are : "<<iRet<<"\n";
+    return 0;
+}
