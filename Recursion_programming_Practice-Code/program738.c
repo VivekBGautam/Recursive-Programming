12786f786979df0a8e6347d3f890daddbef0e2f3
Vivek Gautam <vivekbgautam@gmail.com>
2026-02-20 00:36:36 +0530

    Add files via upload

---
+++ Recursion_programming_Practice-Code/program738.c
@@ -0,0 +1,144 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)                     // To Avoide padding
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+// Call By Value
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : DisplayI
+//  Description   : Displays all elements of the singly linked list.
+//  Input         : PNODE first  →  Pointer to the first node of the linked list
+//  Output        : Prints all node data on screen
+//  Author        : Vivek Bhauraj Gautam
+//  Date          : 05/12/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void DisplayI(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d | -> ",first->data);
+        first = first->next;
+    }
+
+    printf("NULL\n");
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : DisplayR
+//  Description   : Displays all elements of the singly linked list.
+//  Input         : PNODE first  →  Pointer to the first node of the linked list
+//  Output        : Prints all node data on screen
+//  Author        : Vivek Bhauraj Gautam
+//  Date          : 05/12/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void DisplayR(PNODE first)
+{
+    if(first != NULL)
+    {
+        printf("| %d | -> ",first->data);
+        //first = first->next;
+        DisplayR(first->next);
+    }
+    else
+    {
+        printf("NULL\n");
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Count
+//  Description   : Counts the total number of nodes present in the linked list.
+//  Input         : PNODE first  →  Pointer to the first node of the linked list
+//  Output        : Returns integer count of nodes
+//  Author        : Vivek Bhauraj Gautam
+//  Date          : 05/12/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+int Count(PNODE first)
+{
+    static int iCount = 0;
+
+    if(first != NULL)
+    {
+        iCount++;
+        first = first->next;
+        Count(first);
+    }
+    return iCount;
+}
+
+// Call By Address
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : InsertLast
+//  Description   : Inserts a new node at the end of the linked list.
+//  Input         : PPNODE first → Address of first pointer
+//                  int no        → Data to be inserted in the node
+//  Output        : Modifies linked list by adding new node at last position
+//  Author        : Vivek Bhauraj Gautam
+//  Date          : 05/12/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        temp = *first;
+
+        while(temp->next != NULL)           // Type 2
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newn;
+    }
+}
+
+
+
+int main()
+{
+    int iRet = 0;
+    PNODE head = NULL;
+
+    InsertLast(&head,11);
+    InsertLast(&head,21);
+    InsertLast(&head,51);
+    InsertLast(&head,101);
+
+    DisplayR(head);
+
+    iRet = Count(head);
+    printf("Count is : %d\n",iRet);
+    return 0;
+}
